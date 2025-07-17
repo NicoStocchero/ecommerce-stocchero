e132644f260756e26f792567d46b66f4fc3518d1
Nicolás Stocchero <nikostocchero@gmail.com>
2025-07-16 23:40:00 -0300

    Add Loading component for displaying loading indicators with customizable text and size. Includes full-screen and inline styles for flexibility in usage.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,36 @@
+// src/components/Loading.jsx
+import { View, ActivityIndicator, StyleSheet, Text } from "react-native";
+import { colors } from "../global/colors";
+
+const Loading = ({
+  text = "Cargando...",
+  size = "large",
+  fullScreen = true,
+}) => {
+  return (
+    <View style={fullScreen ? styles.fullScreen : styles.inline}>
+      <ActivityIndicator size={size} color={colors.primary} />
+      {text && <Text style={styles.text}>{text}</Text>}
+    </View>
+  );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inline: {
+    paddingVertical: 12,
+    alignItems: "center",
+  },
+  text: {
+    marginTop: 10,
+    fontFamily: "Inter-Regular",
+    fontSize: 16,
+    color: colors.gray,
+  },
+});
